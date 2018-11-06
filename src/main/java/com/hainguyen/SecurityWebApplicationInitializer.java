f6786388d892d5021cf3d24fc5dd37cb50d69ac8
nguyenhai9112 <nguyenhai9112@gmail.com>
2018-11-06 22:38:31 +0700

    tai project

---
+++ src/main/java/com/hainguyen/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.hainguyen;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
